8632b03a140745c4edd182e3e1b4644944ee4662
ScriptorAYA <sultan4jallo@gmail.com>
2026-02-01 21:10:34 +0000

    rest framework

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -44,6 +44,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
 }
 
 ROOT_URLCONF = 'api_project.urls'
