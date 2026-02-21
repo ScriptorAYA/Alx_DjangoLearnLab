31d8c879f69bb920d4015cd7e6e1bc76fe9fc799
ScriptorAYA <sultan4jallo@gmail.com>
2026-02-20 20:34:24 -0500

    My blog task

---
+++ django_blog/blog/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+
+# -----------------------------------------------------------
+# Custom Registration Form
+# Extends Django's built-in UserCreationForm
+# Adds required email field
+# -----------------------------------------------------------
+
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+
+# -----------------------------------------------------------
+# Profile Update Form
+# Allows user to update email and username
+# -----------------------------------------------------------
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,6 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from .forms import RegisterForm, ProfileUpdateForm
 
 
 def home(request):
-    return render(request, 'blog/base.html')
+    return render(request, 'blog/home.html')
+
+# -----------------------------------------------------------
+# Registration View
+# -----------------------------------------------------------
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Auto login after register
+            return redirect('home')
+    else:
+        form = RegisterForm()
+    return render(request, 'blog/register.html', {'form': form})
+
+
+# -----------------------------------------------------------
+# Profile View (Requires Login)
+# -----------------------------------------------------------
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        form = ProfileUpdateForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+    else:
+        form = ProfileUpdateForm(instance=request.user)
+
+    return render(request, 'blog/profile.html', {'form': form})
 
